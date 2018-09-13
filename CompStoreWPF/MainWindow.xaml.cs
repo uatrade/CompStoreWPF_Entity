4990f99e753e03c84d22acaa12db78c70c74ad94
uatrade <piskovtsev@gmail.com>
2018-09-13 18:02:33 +0300

    Add Style

--- CompStoreWPF/MainWindow.xaml.cs
+++ CompStoreWPF/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace CompStoreWPF
     public partial class MainWindow : Window
     {
 
-        List<TempData> list = new List<TempData>();
+        List<TempData> list = new List<TempData>();    //temp для добавления в DATaGrid
         List<TempData> SellList = new List<TempData>();
         List<TempData> CustomerList = new List<TempData>();
 
@@ -33,6 +33,7 @@ namespace CompStoreWPF
             InitializeComponent();
             Uri iconUri = new Uri(@"pack://application:,,,/Resources/IconComp.jpg", UriKind.RelativeOrAbsolute);
             this.Icon = BitmapFrame.Create(iconUri);
+            
             Loaded += MainWindow_Loaded;
             MainComboEquipment.SelectionChanged += MainComboEquipment_SelectionChanged;
 
@@ -78,13 +79,13 @@ namespace CompStoreWPF
                         list.Add(numbers);
                     }
                     else
-                    list.Add(numbers);
+                        list.Add(numbers);
                 }
 
                 ListOfEquipment.ItemsSource = list;
 
                 //tempData.Num = 1;
-                if (tempData.Num >= 1)
+                if (tempData.Num >= 0)
                 {
                     SellList.Add(new TempData(tempData.Title, 1, tempData.Price));
                     CustomerList.Add(new TempData(tempData.Title, 1, tempData.Price));
@@ -101,7 +102,7 @@ namespace CompStoreWPF
                 SellGridView.ItemsSource = SellList;
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
@@ -115,8 +116,7 @@ namespace CompStoreWPF
                 ListOfEquipment.Columns.Clear();
                 ListOfEquipment.Items.Refresh();
                 //list.Clear();
-                
-                
+                             
                 switch (MainComboEquipment.SelectedItem)
                 {
                     case "Процессор":
@@ -148,6 +148,76 @@ namespace CompStoreWPF
                             ListOfEquipment.ItemsSource = list;
                             break;
                         }
+                    case "Оперативная память":
+                        {
+                            list.Clear();
+                            list = new List<TempData>();
+                            using (CompStorEntity db = new CompStorEntity())
+                            {
+                                foreach (var item in db.RAMs)
+                                {
+                                    list.Add(new TempData(item.RAMName, item.NumOfRam, item.RAMPrice));
+                                }
+                            }
+                            ListOfEquipment.ItemsSource = list;
+                            break;
+                        }
+                    case "Жесткий диск":
+                        {
+                            list.Clear();
+                            list = new List<TempData>();
+                            using (CompStorEntity db = new CompStorEntity())
+                            {
+                                foreach (var item in db.HardDisks)
+                                {
+                                    list.Add(new TempData(item.HardDiskName, item.NumOfHardDisk, item.HardDiskPrice));
+                                }
+                            }
+                            ListOfEquipment.ItemsSource = list;
+                            break;
+                        }
+                    case "Видео карта":
+                        {
+                            list.Clear();
+                            list = new List<TempData>();
+                            using (CompStorEntity db = new CompStorEntity())
+                            {
+                                foreach (var item in db.VideoCards)
+                                {
+                                    list.Add(new TempData(item.VideoCardName, item.NumOfVideoCard, item.VideoCardPrice));
+                                }
+                            }
+                            ListOfEquipment.ItemsSource = list;
+                            break;
+                        }
+                    case "Корпус":
+                        {
+                            list.Clear();
+                            list = new List<TempData>();
+                            using (CompStorEntity db = new CompStorEntity())
+                            {
+                                foreach (var item in db.Cases)
+                                {
+                                    list.Add(new TempData(item.CaseName, item.NumOfCase, item.CasePrice));
+                                }
+                            }
+                            ListOfEquipment.ItemsSource = list;
+                            break;
+                        }
+                    case "Монитор":
+                        {
+                            list.Clear();
+                            list = new List<TempData>();
+                            using (CompStorEntity db = new CompStorEntity())
+                            {
+                                foreach (var item in db.Monitors)
+                                {
+                                    list.Add(new TempData(item.MonitorName, item.NumOfMonitor, item.MonitorPrice));
+                                }
+                            }
+                            ListOfEquipment.ItemsSource = list;
+                            break;
+                        }
                 }
             }
             catch (Exception ex)
@@ -164,54 +234,215 @@ namespace CompStoreWPF
 
         private void BtnClear_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                SellGridView.Columns.Clear();
-                ListOfEquipment.Columns.Clear(); 
-                ListOfEquipment.Items.Refresh();
-                SellGridView.Items.Refresh();
-
-                SellList.Clear();
-                list.Clear();
-
-                MainComboEquipment.SelectedIndex =0;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            ClearAll();
         }
-
         private void BtnSell_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 using (CompStorEntity db = new CompStorEntity())
                 {
-                    Processor processor = new Processor();
 
-
-                        foreach (var c in CustomerList)
+#region PROCESSOR
+                    foreach (var item in db.Processors)    //разобраться с удалением
+                    {
+                        foreach (var custom in CustomerList)
                         {
-                            foreach (var item in db.Processors)
+                            if (item.ProcessorName == custom.Title)
                             {
-                                if (item.ProcessorName == c.Title)
+                                if (item.NumOfProcessor > 1)
                                 {
-                                item.NumOfProcessor -= 1;
-                                MessageBox.Show("Списано");
+                                    item.NumOfProcessor -= 1;
+                                    MessageBox.Show($"Товар {item.ProcessorName} успешно продан");
+                                    //TODO Запись в файл
+                                }
+                                else if (item.NumOfProcessor == 1)
+                                {
+                                    db.Processors.Remove(item);
+                                    MessageBox.Show($"Товар успешно продан. {item.ProcessorName} уже закончились на складе");
+                                    //TODO запись в файл
                                 }
                             }
                         }
+                    }
+#endregion
+
+#region MotherBoard
+                    foreach (var itemM in db.Motherboards)
+                        {
+                            foreach (var custom in CustomerList)
+                            {
+                                if (itemM.MotherboardName == custom.Title)
+                                {
+                                    if (itemM.NumOfMotherboard > 1)
+                                    {
+                                        itemM.NumOfMotherboard -= 1;
+                                        MessageBox.Show($"Товар {itemM.MotherboardName} успешно продан");
+                                        //TODO Запись в файл
+                                    }
+                                    else if (itemM.NumOfMotherboard == 1)
+                                    {
+                                        db.Motherboards.Remove(itemM);
+                                        MessageBox.Show($"Товар успешно продан. {itemM.MotherboardName} уже закончились на складе");
+                                        //TODO запись в файл
+                                    }
+                                }
+                            }
+                        }
+#endregion 
+
+#region HardDisk
+                    foreach (var itemM in db.HardDisks)
+                    {
+                        foreach (var custom in CustomerList)
+                        {
+                            if (itemM.HardDiskName == custom.Title)
+                            {
+                                if (itemM.NumOfHardDisk > 1)
+                                {
+                                    itemM.NumOfHardDisk -= 1;
+                                    MessageBox.Show($"Товар {itemM.HardDiskName} успешно продан");
+                                    //TODO Запись в файл
+                                }
+                                else if (itemM.NumOfHardDisk == 1)
+                                {
+                                    db.HardDisks.Remove(itemM);
+                                    MessageBox.Show($"Товар успешно продан. {itemM.HardDiskName} уже закончились на складе");
+                                    //TODO запись в файл
+                                }
+                            }
+                        }
+                    }
+                    #endregion
+
+#region RAM
+                    foreach (var itemM in db.RAMs)
+                    {
+                        foreach (var custom in CustomerList)
+                        {
+                            if (itemM.RAMName == custom.Title)
+                            {
+                                if (itemM.NumOfRam > 1)
+                                {
+                                    itemM.NumOfRam -= 1;
+                                    MessageBox.Show($"Товар {itemM.RAMName} успешно продан");
+                                    //TODO Запись в файл
+                                }
+                                else if (itemM.NumOfRam == 1)
+                                {
+                                    db.RAMs.Remove(itemM);
+                                    MessageBox.Show($"Товар успешно продан. {itemM.RAMName} уже закончились на складе");
+                                    //TODO запись в файл
+                                }
+                            }
+                        }
+                    }
+                    #endregion
+
+#region CASES
+                    foreach (var itemM in db.Cases)
+                    {
+                        foreach (var custom in CustomerList)
+                        {
+                            if (itemM.CaseName == custom.Title)
+                            {
+                                if (itemM.NumOfCase > 1)
+                                {
+                                    itemM.NumOfCase -= 1;
+                                    MessageBox.Show($"Товар {itemM.CaseName} успешно продан");
+                                    //TODO Запись в файл
+                                }
+                                else if (itemM.NumOfCase == 1)
+                                {
+                                    db.Cases.Remove(itemM);
+                                    MessageBox.Show($"Товар успешно продан. {itemM.CaseName} уже закончились на складе");
+                                    //TODO запись в файл
+                                }
+                            }
+                        }
+                    }
+                    #endregion
+
+#region VIDEO
+                    foreach (var itemM in db.VideoCards)
+                    {
+                        foreach (var custom in CustomerList)
+                        {
+                            if (itemM.VideoCardName == custom.Title)
+                            {
+                                if (itemM.NumOfVideoCard > 1)
+                                {
+                                    itemM.NumOfVideoCard -= 1;
+                                    MessageBox.Show($"Товар {itemM.VideoCardName} успешно продан");
+                                    //TODO Запись в файл
+                                }
+                                else if (itemM.NumOfVideoCard == 1)
+                                {
+                                    db.VideoCards.Remove(itemM);
+                                    MessageBox.Show($"Товар успешно продан. {itemM.VideoCardName} уже закончились на складе");
+                                    //TODO запись в файл
+                                }
+                            }
+                        }
+                    }
+                    #endregion
+
+#region MONITOR
+                    foreach (var itemM in db.Monitors)
+                    {
+                        foreach (var custom in CustomerList)
+                        {
+                            if (itemM.MonitorName == custom.Title)
+                            {
+                                if (itemM.NumOfMonitor > 1)
+                                {
+                                    itemM.NumOfMonitor -= 1;
+                                    MessageBox.Show($"Товар {itemM.MonitorName} успешно продан");
+                                    //TODO Запись в файл
+                                }
+                                else if (itemM.NumOfMonitor == 1)
+                                {
+                                    db.Monitors.Remove(itemM);
+                                    MessageBox.Show($"Товар успешно продан. {itemM.MonitorName} уже закончились на складе");
+                                    //TODO запись в файл
+                                }
+                            }
+                        }
+                    }
+                    #endregion
+
                     db.SaveChanges();
+
+                    ClearAll();
                 }
 
-            }
+             }
             catch (Exception ex)
             {
 
                 MessageBox.Show(ex.Message);
             }
            
+        }
+        private void ClearAll()
+        {
+            try
+            {
+                SellGridView.Columns.Clear();
+                ListOfEquipment.Columns.Clear();
+                ListOfEquipment.Items.Refresh();
+                SellGridView.Items.Refresh();
+
+                SellList.Clear();
+                CustomerList.Clear();
+                list.Clear();
+
+                MainComboEquipment.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
     }
